3b3fda875a431ccb141e74d2288e8d7b5ce30f88
njkr <jenkins.raj@gmail.com>
2015-11-05 10:20:09 +0400

    11 5 15

--- node/books.js
+++ node/books.js
@@ -1,4 +1,17 @@
-exports.name = "node js by example ";
-exports.read = function(){
-	console.log("iam reading "+exports.name );
-};
+/* exports.name = 'Node.js by example';
+exports.read = function() {
+console.log('I am reading ' + exports.name);
+} */
+
+
+module.exports=function(){
+	var ratepoint=0;
+	return{
+		rate:function(points){
+			ratepoint = points;
+		},
+		getpoints:function(){
+			return ratepoint;
+		}
+	}
+}
--- node/simpleserver.js
+++ node/simpleserver.js
@@ -1,15 +1,17 @@
 
 
 
-/*var http=require("http");
+var http=require("http");
 http.createServer(function(req,res){
 	res.writeHead(200,{"Content-Type":"text/plain"});
-	res.end("Hellow world \n");
+	res.end(function(){});
 }).listen(9000,'127.0.0.1');
-console.log("server is stsrted"); */
+console.log("server is stsrted"); 
 
 
 
-var book = require('./books.js');
+ /* var book = require('./books.js');
 console.log('Name: ' + book.name);
-book.read();
+book.read(); */
+
+
